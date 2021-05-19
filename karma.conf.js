ada4241a9b40c6d4cc455f4a105c50b5bc5fceb0
lsanchez <lionani07@gmail.com>
2021-05-18 23:51:47 -0300

    set project name

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/material-template-lio'),
+      dir: require('path').join(__dirname, './coverage/algamoney-ui'),
       subdir: '.',
       reporters: [
         { type: 'html' },
